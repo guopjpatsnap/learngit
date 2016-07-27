f7f4e69bf9ac874ef9d2a27acab26fdecbab5a14
guopj <guopengjie@patsnap.com>
2016-07-27 18:13:04 +0800

    test file change

--- test.php
+++ test.php
@@ -4,4 +4,12 @@
  * User: Administrator
  * Date: 2016/7/27
  * Time: 17:52
- */ 
+ */
+
+echo "Git is a distributed version control system.Git is free software.";
+
+echo "change file!";
+echo "change file!";
+echo "change file!";
+echo "change file!";
+echo "change file!";
