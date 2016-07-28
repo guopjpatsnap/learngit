17abc2a634986f30cbf8d7650308e3051910ae74
guopj <guopengjie@patsnap.com>
2016-07-28 09:33:16 +0800

    how stage works

---
+++ license.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/28
+ * Time: 9:30
+ */
+
+echo "license";
--- test.php
+++ test.php
@@ -13,3 +13,5 @@ echo "change file!";
 echo "change file!";
 echo "change file!";
 echo "change file!";
+
+echo "Git is a distributed version control system.Git is free software distributed under the GPL.Git has a mutable index called stage.";
